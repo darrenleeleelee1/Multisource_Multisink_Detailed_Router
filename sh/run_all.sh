435f6276e4e7b78ea87f5a96d4eb76c39ab66e9c
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2023-02-09 20:37:28 +0800

    failed verify

--- sh/run_all.sh
+++ sh/run_all.sh
@@ -1,12 +1,32 @@
 # build
 make clean && make -j16
+
 # run
 for i in {0..9}; do
     echo "Running case test$i.txt"
     ./router ./case/test$i.txt ./out/test$i.txt
 done
+
+# verify
+if [ "$1" == "--verify" ]; then
+    for i in {0..9}; do
+        echo "Running case test$i.txt."
+        ./verifier ./out/test$i.txt > ./out/log$i.txt
+    done
+    for i in {0..9}; do
+        if grep -q "Error" ./out/log$i.txt; then
+            echo "Error found in case test$i.txt."
+            echo "Please run the following code to check the log."
+            echo "./verifier ./out/test$i.txt"
+            exit 1
+        else
+            echo "Passed."
+        fi
+    done
+fi
+
+
 # draw
-if [ $1 == "--draw" ]
-then
+if [ "$2" == "--draw" ]; then
     python3 visual.py --dir ./out
 fi
